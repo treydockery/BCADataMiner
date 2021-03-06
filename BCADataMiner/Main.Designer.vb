19451a65bc96076f54d4f513b345d724f8f0a0a8
treydockery <trey@dockeryfamily.com>
2021-03-06 14:44:07 -0600

    fixed Company revenue and Earnings Date

--- BCADataMiner/Main.Designer.vb
+++ BCADataMiner/Main.Designer.vb
@@ -28,16 +28,12 @@ Partial Class Main
         Me.Label1 = New System.Windows.Forms.Label()
         Me.txtActivityLog = New System.Windows.Forms.TextBox()
         Me.MainWebBrowser = New System.Windows.Forms.WebBrowser()
-        Me.cbOnlyRunRelativeStrength = New System.Windows.Forms.CheckBox()
-        Me.cbForceRS = New System.Windows.Forms.CheckBox()
-        Me.cbOnlyRunPerpetualIncome = New System.Windows.Forms.CheckBox()
-        Me.cbRuleOfSix = New System.Windows.Forms.CheckBox()
         Me.SuspendLayout()
         '
         'btnGetLegacyScores
         '
         Me.btnGetLegacyScores.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnGetLegacyScores.Location = New System.Drawing.Point(989, 139)
+        Me.btnGetLegacyScores.Location = New System.Drawing.Point(982, 37)
         Me.btnGetLegacyScores.Margin = New System.Windows.Forms.Padding(4)
         Me.btnGetLegacyScores.Name = "btnGetLegacyScores"
         Me.btnGetLegacyScores.Size = New System.Drawing.Size(112, 31)
@@ -86,61 +82,11 @@ Partial Class Main
         Me.MainWebBrowser.Size = New System.Drawing.Size(1192, 581)
         Me.MainWebBrowser.TabIndex = 16
         '
-        'cbOnlyRunRelativeStrength
-        '
-        Me.cbOnlyRunRelativeStrength.AutoSize = True
-        Me.cbOnlyRunRelativeStrength.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.cbOnlyRunRelativeStrength.Location = New System.Drawing.Point(925, 37)
-        Me.cbOnlyRunRelativeStrength.Name = "cbOnlyRunRelativeStrength"
-        Me.cbOnlyRunRelativeStrength.Size = New System.Drawing.Size(223, 24)
-        Me.cbOnlyRunRelativeStrength.TabIndex = 17
-        Me.cbOnlyRunRelativeStrength.Text = "Only Run Relaitive Strength"
-        Me.cbOnlyRunRelativeStrength.UseVisualStyleBackColor = True
-        '
-        'cbForceRS
-        '
-        Me.cbForceRS.AutoSize = True
-        Me.cbForceRS.Checked = True
-        Me.cbForceRS.CheckState = System.Windows.Forms.CheckState.Checked
-        Me.cbForceRS.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.cbForceRS.Location = New System.Drawing.Point(924, 104)
-        Me.cbForceRS.Name = "cbForceRS"
-        Me.cbForceRS.Size = New System.Drawing.Size(281, 24)
-        Me.cbForceRS.TabIndex = 18
-        Me.cbForceRS.Text = "Force Running of Relaitive Strength"
-        Me.cbForceRS.UseVisualStyleBackColor = True
-        '
-        'cbOnlyRunPerpetualIncome
-        '
-        Me.cbOnlyRunPerpetualIncome.AutoSize = True
-        Me.cbOnlyRunPerpetualIncome.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.cbOnlyRunPerpetualIncome.Location = New System.Drawing.Point(924, 58)
-        Me.cbOnlyRunPerpetualIncome.Name = "cbOnlyRunPerpetualIncome"
-        Me.cbOnlyRunPerpetualIncome.Size = New System.Drawing.Size(222, 24)
-        Me.cbOnlyRunPerpetualIncome.TabIndex = 19
-        Me.cbOnlyRunPerpetualIncome.Text = "Only Run Perpetual Income"
-        Me.cbOnlyRunPerpetualIncome.UseVisualStyleBackColor = True
-        '
-        'cbRuleOfSix
-        '
-        Me.cbRuleOfSix.AutoSize = True
-        Me.cbRuleOfSix.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.cbRuleOfSix.Location = New System.Drawing.Point(924, 81)
-        Me.cbRuleOfSix.Name = "cbRuleOfSix"
-        Me.cbRuleOfSix.Size = New System.Drawing.Size(138, 24)
-        Me.cbRuleOfSix.TabIndex = 20
-        Me.cbRuleOfSix.Text = "Run Rule of Six"
-        Me.cbRuleOfSix.UseVisualStyleBackColor = True
-        '
         'Main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1221, 822)
-        Me.Controls.Add(Me.cbRuleOfSix)
-        Me.Controls.Add(Me.cbOnlyRunPerpetualIncome)
-        Me.Controls.Add(Me.cbForceRS)
-        Me.Controls.Add(Me.cbOnlyRunRelativeStrength)
         Me.Controls.Add(Me.MainWebBrowser)
         Me.Controls.Add(Me.btnGetLegacyScores)
         Me.Controls.Add(Me.Label2)
@@ -161,8 +107,4 @@ Partial Class Main
     Friend WithEvents Label1 As Label
     Friend WithEvents txtActivityLog As TextBox
     Friend WithEvents MainWebBrowser As WebBrowser
-    Friend WithEvents cbOnlyRunRelativeStrength As CheckBox
-    Friend WithEvents cbForceRS As CheckBox
-    Friend WithEvents cbOnlyRunPerpetualIncome As CheckBox
-    Friend WithEvents cbRuleOfSix As CheckBox
 End Class
